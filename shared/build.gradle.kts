aaa8ae9be00e787eaa409611aa9b2f1fe0b523d3
pathiphon <pathiphon@ch7.com>
2023-08-01 18:17:17 +0700

    setup kamel image.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -45,6 +45,8 @@ kotlin {
                 implementation(libs.io.ktor.client.core)
                 implementation(libs.io.ktor.client.content.negotiation)
                 implementation(libs.io.ktor.serialization.kotlinx.json)
+
+                implementation(libs.media.kamel.image)
             }
         }
         val androidMain by getting {
@@ -83,11 +85,11 @@ android {
         targetSdk = (findProperty("android.targetSdk") as String).toInt()
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlin {
-        jvmToolchain(11)
+        jvmToolchain(17)
     }
 }
 
--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,6 +1,7 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -13,7 +14,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import domain.usecases.greeting.GetGreetingUseCase
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.koinInject
@@ -25,15 +29,25 @@ fun App() {
         var greetingText by remember { mutableStateOf("Hello, World!") }
         var showImage by remember { mutableStateOf(false) }
         var data by remember { mutableStateOf("") }
+        var imageUrl by remember { mutableStateOf<String?>(null) }
         val greetingUseCase = koinInject<GetGreetingUseCase>()
 
         LaunchedEffect(Unit) {
             val result = greetingUseCase.execute()
             data = result.toString()
+            imageUrl = result.imageUrl
         }
 
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Text(data)
+            imageUrl?.let {
+                KamelImage(
+                    asyncPainterResource(data = it),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.aspectRatio(1f)
+                )
+            }
             Button(onClick = {
                 greetingText = "Hello, ${getPlatformName()}"
                 showImage = !showImage
